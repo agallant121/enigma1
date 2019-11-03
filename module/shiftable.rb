115189516bf906b9e65521fb9d97a497c231bfb4
Alex Gallant <agallant1201@gmail.com>
2019-11-03 12:18:14 -0700

    Add date shift in module

--- module/shiftable.rb
+++ module/shiftable.rb
@@ -12,6 +12,16 @@ class Shiftable
   d = key[3..4].to_i
 
   [a, b, c, d]
-end
+  end
+
+  def date_shift(date)
+    date_squared = (date.to_i ** 2).to_s
+    a = date_squared[-4].to_i
+    b = date_squared[-3].to_i
+    c = date_squared[-2].to_i
+    d = date_squared[-1].to_i
+
+    [a, b, c, d]
+  end
 
 end
